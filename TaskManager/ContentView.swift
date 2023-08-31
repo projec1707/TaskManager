dbe7126d3ee2c93975a9048cc9f2c4621bfcc3b4
projec1707 <pwroma@gmail.com>
2023-08-31 12:13:37 +0300

    Initial Commit

---
+++ TaskManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskManager
+//
+//  Created by Roman Riepa on 31.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
